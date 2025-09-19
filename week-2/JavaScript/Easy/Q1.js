32a8c7b502a9e3bdb613eed5548bf9856234ca3b
Kripa-Acharya <84564434+Kripa-Acharya@users.noreply.github.com>
2025-09-19 12:43:38 +0545

    Create Q1.js

---
+++ week-2/JavaScript/Easy/Q1.js
@@ -0,0 +1,21 @@
+/*
+  Write a function `isAnagram` which takes 2 parameters and returns true/false if those are anagrams or not.
+  What's Anagram?
+  - A word, phrase, or name formed by rearranging the letters of another, such as spar, formed from rasp.
+*/
+
+function isAnagram(str1, str2) {
+  const sortedstr1 = str1.toLowerCase().split('').sort().join('');
+  const sortedstr2 = str2.toLowerCase().split('').sort().join('');
+
+  if (sortedstr1==sortedstr2){
+    return true;
+    
+  }
+  else{
+    return false;
+    
+  }
+}
+const a = isAnagram("rohan","ohran")
+console.log(a);
