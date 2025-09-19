aa0660386034ba6a89e374880acb820c1c1568d3
Kripa-Acharya <84564434+Kripa-Acharya@users.noreply.github.com>
2025-09-19 12:44:39 +0545

    Create Q3.js

---
+++ week-2/JavaScript/Easy/Q3.js
@@ -0,0 +1,20 @@
+/*
+  Write a function `findLargestElement` that takes an array of numbers and returns the largest element.
+  Example:
+  - Input: [3, 7, 2, 9, 1]
+  - Output: 9
+*/
+
+function findLargestElement(numbers) {
+    let largestNum = numbers[0];
+    for (let i = 0; i < numbers.length ; i++) {
+        if(numbers[i]>largestNum){
+            largestNum = numbers[i];
+        }        
+    }
+    return largestNum;
+}
+
+numbers = [2,3,5,9,7]
+console.log(findLargestElement(numbers));
+
